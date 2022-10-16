bd13883f23cbfcff79c975bc6c59fed60ae8dc8a
mrchosen <chosenvincent@gmail.com>
2022-10-16 16:03:05 -0700

    First Commit

---
+++ javascrip.js
@@ -0,0 +1,7 @@
+const names = ["john", "cat", "flip", "sky"];
+
+for (var i = 0; i < names.length; i++){
+    console.log("the names are: " + names[i]);
+}
+
+document.getElementById('demo').innerHTML = "Helloooooo";
